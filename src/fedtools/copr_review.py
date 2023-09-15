bd56ba603612cc5fe4f36809eff4c9ca2adf0931
blinxen <h-k-81@hotmail.com>
2023-09-16 00:54:08 +0200

    [copr-review] Add f39 to chroots

--- src/fedtools/copr_review.py
+++ src/fedtools/copr_review.py
@@ -17,6 +17,8 @@ def build(args: Namespace):
             "--chroot",
             "fedora-rawhide-x86_64",
             "--chroot",
+            "fedora-39-x86_64",
+            "--chroot",
             "fedora-38-x86_64",
             "--chroot",
             "fedora-37-x86_64",
